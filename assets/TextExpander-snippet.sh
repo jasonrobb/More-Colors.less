07d78455291956d67f28d38ad7bd405e9da2b74e
Jason Robb <jason@jasonrobb.com>
2012-05-01 22:11:55 -0400

    Okay, she's just about done.

--- assets/TextExpander-snippet.sh
+++ assets/TextExpander-snippet.sh
@@ -8,30 +8,3 @@ until [ $numberA == %fill:numbers% ]; do
     numberA=$((numberA + 1))
     numberB=$((numberB - 1))
 done
-
-# numberA=0
-# numberB=%fill:cycles%
-# 
-# until [ $numberA == %fill:cycles% ]; do
-#     echo "-$numberA $numberA 0 mix(@color, @black, 90%),"
-#     numberA=$((numberA + 1))
-#     numberB=$((numberB - 1))
-# done
-# 
-# 
-# number=1
-# 
-# until [ $number == %fill:cycles% ]; do
-#     echo "$number"
-#     number=$((number + 1))
-# done
-#
-# 
-# numberA=0
-# numberB=%fill:numbers%
-# 
-# until [ $numberA == %fill:numbers% ]; do
-#     echo "@black$numberB: fadeout(@black, $numberA%);"
-#     numberA=$((numberA + 1))
-#     numberB=$((numberB - 1))
-# done
